4b49ccd824de9b93050e4691656ba8ac7824bbac
Nikita Gorbunov <nert138@gmail.com>
2024-10-12 22:03:55 +0300

    Add README and hello.pas

---
+++ hello.pas
@@ -0,0 +1,10 @@
+﻿begin
+  var language := ReadString('What language do you speak?:');
+  case language of
+    'Deutsch' : Print('Hallo!');
+    'Русский' : Print('Привет!');
+    'English' : Print('Hellow');
+    'Кыргызча' : Print('Салам');
+    '日本語' : Print('こんにちは');
+  end;
+end.
